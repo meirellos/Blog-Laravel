fcdfd4c14ea2308a6fd7cdebb24ce5dc1a02f85c
Vinicius Meirelles <72823459+meirellos@users.noreply.github.com>
2023-06-20 23:56:53 -0300

    .

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,16 +1,22 @@
 <div class="py-4 text-center">
-   
-  </div>
 
-  <hr class="m-0" />
+</div>
 
-  <div class="text-center py-4 align-items-center">
-    
-  </div>
+<hr class="m-0" />
 
-  <!-- Copyright -->
-  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-    © 2020 Copyright:
-    <a class="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a>
-  </div>
-  <!-- Copyright -->
+<div class="text-center py-4 align-items-center">
+
+</div>
+
+<!-- Copyright -->
+<div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    <a class="text-dark" href="https://github.com/meirellos" rel="nofollow" target="_blank">
+        <i class="fab fa-github"></i> Github
+    </a>
+
+    <a class="text-dark" href="https://www.linkedin.com/in/vinicius-meirelles-4981501b4/">
+        <i class="fab fa-linkedin"></i>
+        Vinicius Meirelles
+    </a>
+</div>
+<!-- Copyright -->
